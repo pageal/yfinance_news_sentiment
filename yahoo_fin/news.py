edeb3b40959b9143743c09bd5d4a3cf66f820d36
Pavel Gesyuk <pageal@gmail.com>
2021-05-06 09:29:02 +0300

    export to csv

--- yahoo_fin/news.py
+++ yahoo_fin/news.py
@@ -9,6 +9,9 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import pandas as pd
 import nltk
 
+csvDataFrameColumns = None
+csvDataArray = []
+
 yf_rss_ticket_url = 'https://feeds.finance.yahoo.com/rss/2.0/headline?s=%s&region=US&lang=en-US'
 yf_rss_url = 'https://finance.yahoo.com/news/rssindex'
 
@@ -36,6 +39,7 @@ def print_scored_news(entry):
     print("    SCORE compound: {} (neg: {} new: {} pos: {})".format(entry['compound'], entry['neg'], entry['neu'], entry['pos']))
 
 def scan_yf_news(score_threshold = 0.3, ticker = 'INTC'):
+    global csvDataFrameColumns
     if(ticker != None):
         entries = get_yf_ticker_rss(ticker)
     else:
@@ -55,6 +59,8 @@ def scan_yf_news(score_threshold = 0.3, ticker = 'INTC'):
         news = []
         if (ticker != None):
             news.append(ticker)
+        else:
+            news.append("")
         news.append(dt.date())
         news.append(dt.time())
         news.append(entry['title'])
@@ -64,10 +70,7 @@ def scan_yf_news(score_threshold = 0.3, ticker = 'INTC'):
         i = i + 1
 
 
-    if (ticker != None):
-        columns = ['ticker', 'date', 'time', 'headline']
-    else:
-        columns = ['date', 'time', 'headline']
+    columns = ['ticker', 'date', 'time', 'headline']
 
     # Instantiate the sentiment intensity analyzer
     nltk.download("vader_lexicon")
@@ -93,15 +96,31 @@ def scan_yf_news(score_threshold = 0.3, ticker = 'INTC'):
     parsed_and_scored_news['date'] = pd.to_datetime(parsed_and_scored_news.date).dt.date
 
     parsed_and_scored_news.head()
+    if csvDataFrameColumns is None:
+        csvDataFrameColumns = parsed_and_scored_news.columns
     for index, news_item in parsed_and_scored_news.iterrows():
         # DEBUG PRINT
         # print(str(news_item['compound']))
         if news_item['compound'] > score_threshold or news_item['compound'] < (-1*score_threshold):
+            csvDataArray.append(news_item.tolist())
             if (ticker != None):
                 print_scored_ticker_news(news_item)
             else:
                 print_scored_news(news_item)
 
+
+from tickers import tickers_pg
+
 if __name__ == '__main__':
-    scan_yf_news(score_threshold=0.5, ticker=None)
-    scan_yf_news(ticker='INTC')
+    dt_now = datetime.now()
+    dt_string = dt_now.strftime("%Y%m%d_%H%M%S")
+
+    scan_yf_news(score_threshold=0.65, ticker=None)
+    #scan_yf_news(ticker='INTC')
+
+    for ticker in tickers_pg:
+        scan_yf_news(score_threshold=0.65, ticker=ticker)
+    csvDataFrame = pd.DataFrame(csvDataArray, columns=csvDataFrameColumns)
+    csvDataFrame.to_csv("C:/Users/USER/Downloads/yahoo_fin_news/" +dt_string+".csv")
+
+
